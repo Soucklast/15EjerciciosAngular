9a54dc6ba662fb480e3a3c1e71d4388d670c2f3f
Arisaí López <arloma49@gmail.com>
2025-07-14 13:41:53 -0600

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://github.com/Soucklast/15EjerciciosAngular',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
